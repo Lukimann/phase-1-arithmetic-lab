4f10f66c2f23d4e0b99ada1051893264c651d2e2
Luka Wanyama <lukewanyama5@gmail.com>
2023-12-13 12:50:04 +0300

    AAlmost done

--- index.js
+++ index.js
@@ -1,2 +1,27 @@
 
 // Write your code here
+
+let multiply;
+let num1 = 31;
+let num2 = 2;
+
+multiply = num1 * num2;
+console.log(multiply);
+
+let random = Math.ceil(Math.random) 
+
+let mod;
+let num3 = 16;
+let num4 = 4;
+
+mod = num3 / num4;
+console.log(mod)
+
+let max;
+let num5 = 10;
+let num6 = 15;
+let num7 = 20;
+
+max = Math.max(num5, num6, num7);
+console.log(max)
+
